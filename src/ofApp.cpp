3865fbfbc46bc85bc0068155c3830e02181aa470
ColeJohnston <cole.e.johnston16@gmail.com>
2025-02-02 16:43:55 -0500

    Added bubble sort, insertion sort and merge sort

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,17 +3,17 @@
 
 vector<int> numbers;
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 //200 - numbers[i] - 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 	int temp = 0;
 	int midpointOfNext = 0;
 	for (size_t i = 0; i < numbers.size(); i++) {
@@ -26,9 +26,28 @@ void ofApp::draw(){
 		temp = numbers[i];
 	}
 }
+void merge(std::vector<int>& arr, int left, int mid, int right) {
+	std::vector<int> temp;
+	int i = left, j = mid + 1;
+	while (i <= mid && j <= right) {
+		if (arr[i] <= arr[j]) temp.push_back(arr[i++]);
+		else temp.push_back(arr[j++]);
+	}
+	while (i <= mid) temp.push_back(arr[i++]);
+	while (j <= right) temp.push_back(arr[j++]);
+	for (int k = 0; k < temp.size(); k++) arr[left + k] = temp[k];
+}
+void mergeSort(std::vector<int>& arr, int left, int right) {
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+		mergeSort(arr, left, mid);
+		mergeSort(arr, mid + 1, right);
+		merge(arr, left, mid, right);
+	}
+}
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 	switch (key) {
 	case 'r':
 		numbers.clear();
@@ -36,55 +55,79 @@ void ofApp::keyPressed(int key){
 			numbers.push_back(ofRandom(10, 101));
 		}
 		break;
+	case 'b':
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5 - i - 1; j++) {
+				if (numbers[j] > numbers[j + 1]) {
+					int t = numbers[j];
+					numbers[j] = numbers[j + 1];
+					numbers[j + 1] = t;
+				}
+			}
+		}
+		break;
+	case 'i':
+		for (size_t i = 1; i < numbers.size(); i++) {
+			int key = numbers[i];
+			int j = i - 1;
+			while (j >= 0 && numbers[j] > key) {
+				numbers[j + 1] = numbers[j];
+				j--;
+			}
+			numbers[j + 1] = key;
+		}
+		break;
+	case 'm':
+		mergeSort(numbers, 0, numbers.size() - 1);
+		break;
 	}
 }
-
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
