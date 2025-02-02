d1caa67e195a28fdb655421e5c2403d8106328ec
ColeJohnston <cole.e.johnston16@gmail.com>
2025-02-02 17:12:49 -0500

    Added merge sort and quick sort

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -45,7 +45,26 @@ void mergeSort(std::vector<int>& arr, int left, int right) {
 		merge(arr, left, mid, right);
 	}
 }
+int partition(std::vector<int>& arr, int low, int high) {
+	int pivot = arr[high];
+	int i = low - 1;
+	for (int j = low; j < high; j++) {
+		if (arr[j] < pivot) {
+			i++;
+			std::swap(arr[i], arr[j]);
+		}
+	}
+	std::swap(arr[i + 1], arr[high]);
+	return i + 1;
+}
 
+void quickSort(std::vector<int>& arr, int low, int high) {
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
 	switch (key) {
@@ -79,6 +98,9 @@ void ofApp::keyPressed(int key) {
 		break;
 	case 'm':
 		mergeSort(numbers, 0, numbers.size() - 1);
+		break;
+	case 'q':
+		quickSort(numbers, 0, numbers.size() - 1);
 		break;
 	}
 }
