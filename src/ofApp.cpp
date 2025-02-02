86a7181dc4f6408b995de770416f18b10aa88c92
ColeJohnston <cole.e.johnston16@gmail.com>
2025-02-02 13:53:14 -0500

    Added the randomizer and bubbles to match the size of the numbers

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,5 +1,7 @@
 #include "ofApp.h"
+#include <vector>
 
+vector<int> numbers;
 //--------------------------------------------------------------
 void ofApp::setup(){
 
@@ -9,15 +11,32 @@ void ofApp::setup(){
 void ofApp::update(){
 
 }
-
+//200 - numbers[i] - 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	int temp = 0;
+	int midpointOfNext = 0;
+	for (size_t i = 0; i < numbers.size(); i++) {
+		midpointOfNext += 150;
+		midpointOfNext -= 100 - (temp + numbers[i]);
+		ofSetColor(255, 0, 0);
+		ofDrawCircle(midpointOfNext, 300, numbers[i]);
+		ofSetColor(255);
+		ofDrawBitmapString(numbers[i], midpointOfNext, 300);
+		temp = numbers[i];
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	switch (key) {
+	case 'r':
+		numbers.clear();
+		for (int i = 0; i < 5; i++) {
+			numbers.push_back(ofRandom(10, 101));
+		}
+		break;
+	}
 }
 
 //--------------------------------------------------------------
